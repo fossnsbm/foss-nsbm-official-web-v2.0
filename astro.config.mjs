6d92fcfe7455770fb2896348db422a0059cc9e9b
yohanmanohara <warushayohan80@gmail.com>
2024-11-13 23:27:17 +0530

    make register form backend astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: "https://fossnsbm.org",
   integrations: [
     tailwind(),
---
+++ src/lib/neon.ts
@@ -0,0 +1,5 @@
+
+
+import { neon } from '@neondatabase/serverless';
+
+export const sql = neon(import.meta.env.NEON_DATABASE_URL);
