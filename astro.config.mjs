1d55b4a4cdebc096a7fbcc05f2d5ed455d1f8a1b
yohanmanohara <warushayohan80@gmail.com>
2024-11-16 13:36:04 +0530

    make event pages for volunteers

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/old-page': '/new-page'
+  },
   output: 'server',  // or 'hybrid' for hybrid rendering
   adapter: vercel(),
   site: "https://fossnsbm.org",
--- src/middleware.js
+++ src/middleware.js
@@ -4,12 +4,12 @@ import { getSession } from "auth-astro/server"; // Assuming you use auth-astro f
 export const onRequest = defineMiddleware(async (context, next) => {
   const { request } = context;
   
-  // Check if the requested path is '/register'
+  
   if (request.url.endsWith('/register')) {
-    // Retrieve the session from the request
+   
     const session = await getSession(request);
 
-    // If no session exists, redirect to login page
+  
     if (!session) {
       return new Response("Redirecting to login...", {
         status: 302,
@@ -17,6 +17,20 @@ export const onRequest = defineMiddleware(async (context, next) => {
       });
     }
   }
+
+  if (request.url.endsWith('/event')) {
+    
+    const session = await getSession(request);
+
+    
+    if (!session) {
+      return new Response("Redirecting to login...", {
+        status: 302,
+        headers: { Location: '/login' },
+      });
+    }
+  }
+
 
   // Allow request to continue if session exists or the path is not '/register'
   return next();
