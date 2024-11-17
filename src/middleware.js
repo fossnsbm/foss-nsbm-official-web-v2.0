728908654693b81ad157982bb4f531a5c6dec89d
yohanmanohara <warushayohan80@gmail.com>
2024-11-17 21:18:50 +0530

    fix erros

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 import { getSession } from "auth-astro/server"; 
 
+
 export const onRequest = defineMiddleware(async (context, next) => {
   const { request } = context;
   
@@ -32,15 +33,16 @@ export const onRequest = defineMiddleware(async (context, next) => {
   }
   if (request.url.endsWith('/dashboard')) {
     const session = await getSession(request);
-  
-    if (!session ||session.user?.email !== 'warushayohan80@gmail.com') {
+    const adminEmail = import.meta.env.EMAIL_ADMIN;  // Access the environment variable
+
+    if (!session || session.user?.email !== adminEmail) {
       return new Response("Redirecting to login...", {
         status: 302,
         headers: { Location: '/login' },
       });
     }
- 
   }
+
 
   return next();
 });
