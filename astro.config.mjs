e84c4212a51dfe6ee6d745daa330c2e3b64eb8ff
libresoul <contact@damindu.me>
2026-07-04 02:13:39 +0530

    chore!: drop auth components, upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from 'astro/config'
 import partytown from '@astrojs/partytown'
-import tailwind from '@astrojs/tailwind'
-import auth from 'auth-astro'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/old-page': '/new-page',
   },
-  output: 'server', // or 'hybrid' for hybrid rendering
+
+  // or 'hybrid' for hybrid rendering
+  output: 'server',
+
   adapter: vercel(),
   site: 'https://fossnsbm.org',
+
   integrations: [
-    auth(),
-    tailwind(),
     sitemap(),
     partytown({
       config: {
@@ -22,9 +23,14 @@ export default defineConfig({
       },
     }),
   ],
+
   middleware: [
     {
       src: './src/middleware.ts', // Ensure the correct path here
     },
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
